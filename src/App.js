9625d49b55b05be2c898f9b67783b951a6a5fd56
romank123 <67678390+romank123@users.noreply.github.com>
2022-10-23 23:24:56 +0300

    add homework-4-listings

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './css/App.css';
-import Stars from './Stars';
+import Listing from './Listing';
+import etsy from './data/etsy';
 
 function App() {
-  return (
-    <div>
-      <Stars/>
-      <Stars count={0}/>
-      <Stars count={1}/>
-      <Stars count={2}/>
-      <Stars count={3}/>
-      <Stars count={4}/>
-      <Stars count={5}/>
-      <Stars count={6}/>
-    </div>
-  );
+  return <Listing items={etsy}/>;
 }
 
 export default App;
