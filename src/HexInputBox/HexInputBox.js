9ca62554d74b505ce97d23b639436b9801b63b7b
romank123 <67678390+romank123@users.noreply.github.com>
2022-11-03 22:54:39 +0300

    добавил валидацию для ввода данных

--- src/HexInputBox/HexInputBox.js
+++ src/HexInputBox/HexInputBox.js
@@ -1,14 +1,23 @@
-import './HexInputBox.css';
-import React from 'react';
-import PropTypes from 'prop-types';
+import "./HexInputBox.css";
+import React from "react";
+import PropTypes from "prop-types";
 
 export default function HexInputBox(props) {
-  const onChange = (evt) => { props.onChange(evt.target.value) };
+  const onChange = (evt) => {
+    props.onChange(evt.target.value);
+  };
 
-  return <input className='hex-input-box' value={props.value} onChange={onChange}/>;
+  return (
+    <input
+      className='hex-input-box'
+      value={props.value}
+      onChange={onChange}
+      maxLength={7}
+    />
+  );
 }
 
 HexInputBox.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-}
+};
