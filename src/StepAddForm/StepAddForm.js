2f0d605e3e734e3ac54d2b222a00901011d366fa
romank123 <67678390+romank123@users.noreply.github.com>
2022-11-03 23:51:57 +0300

    добавил валидацию на инпут даты и инпут расстояния

--- src/StepAddForm/StepAddForm.js
+++ src/StepAddForm/StepAddForm.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './StepAddForm.css';
-
+import React from "react";
+import PropTypes from "prop-types";
+import "./StepAddForm.css";
 export default function StepAddForm(props) {
   const { form, onSubmit, onChange } = props;
 
@@ -13,25 +12,37 @@ export default function StepAddForm(props) {
   const handleSubmit = (evt) => {
     evt.preventDefault();
     onSubmit();
-  }
+  };
 
   return (
     <form className='step-add-form' onSubmit={handleSubmit}>
       <div className='step-add-form-date'>
-        <label htmlFor='date'>{'Дата (ДД.ММ.ГГ)'}</label>
-        <input id='date' name='date' value={form.date} onChange={handleChange}/>
+        <label htmlFor='date'>{"Дата (ДД.ММ.ГГ)"}</label>
+        <input
+          id='date'
+          name='date'
+          type='date'
+          value={form.date}
+          onChange={handleChange}
+        />
       </div>
       <div className='step-add-form-distance'>
-        <label htmlFor='distance'>{'Пройдено км'}</label>
-        <input id='distance' name='distance' value={form.distance} onChange={handleChange}/>
+        <label htmlFor='distance'>{"Пройдено км"}</label>
+        <input
+          id='distance'
+          name='distance'
+          type='number'
+          value={form.distance}
+          onChange={handleChange}
+        />
       </div>
       <button type='submit'>Ok</button>
     </form>
-  )
+  );
 }
 
 StepAddForm.propTypes = {
   form: PropTypes.object.isRequired,
   onSubmit: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
-}
+};
--- src/Steps/Steps.js
+++ src/Steps/Steps.js
@@ -1,55 +1,67 @@
-import React from 'react';
-import StepModel from './StepModel';
-import StepAddForm from '../StepAddForm/StepAddForm';
-import StepList from '../StepList/StepList';
-import moment from 'moment';
-import './Steps.css';
+import React from "react";
+import StepModel from "./StepModel";
+import StepAddForm from "../StepAddForm/StepAddForm";
+import StepList from "../StepList/StepList";
+import moment from "moment";
+import "./Steps.css";
 
 export default function Steps(props) {
-  const [ steps, setSteps ] = React.useState([]);
-  const [ editingStepID, setEditingStepID ] = React.useState();
-  const [ form, setForm ] = React.useState({ date: '', distance: '' });
+  const [steps, setSteps] = React.useState([]);
+  const [editingStepID, setEditingStepID] = React.useState();
+  const [form, setForm] = React.useState({ date: "", distance: "" });
 
   const handleChange = (name, value) => {
-    setForm(prevForm => ({...prevForm, [name]: value}));
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
   };
 
   const handleSubmit = () => {
     const { distance } = form;
-    const mDate = moment(form.date, 'DD.MM.YY', true);
+    const mDate = moment(form.date, "YYYY-MM-DD", true);
     if (!mDate.isValid()) return;
     const date = mDate.toDate();
 
     if (editingStepID) {
       const oldDate = steps.find((o) => o.id === editingStepID).date;
-      setSteps((prevSteps) => prevSteps.map((o) => {
-        if (o.date.valueOf() === oldDate.valueOf()) return new StepModel(date, Number(distance));
-        return o;
-      }));
+      setSteps((prevSteps) =>
+        prevSteps.map((o) => {
+          if (o.date.valueOf() === oldDate.valueOf())
+            return new StepModel(date, Number(distance));
+          return o;
+        })
+      );
     } else {
       if (steps.find((o) => o.date.valueOf() === date.valueOf())) {
-        setSteps((prevSteps) => prevSteps.map((o) => {
-          if (o.date.valueOf() === date.valueOf()) return new StepModel(date, Number(distance) + o.distance);
-          return o;
-        }));
+        setSteps((prevSteps) =>
+          prevSteps.map((o) => {
+            if (o.date.valueOf() === date.valueOf())
+              return new StepModel(date, Number(distance) + o.distance);
+            return o;
+          })
+        );
       } else {
-        setSteps((prevSteps) => [...prevSteps, new StepModel(date, Number(distance))]);
+        setSteps((prevSteps) => [
+          ...prevSteps,
+          new StepModel(date, Number(distance)),
+        ]);
       }
     }
-    
-    setForm({ date: '', distance: '' });
+
+    setForm({ date: "", distance: "" });
     setEditingStepID(null);
-  }
+  };
 
   const handleRemove = (id) => {
     setSteps((prevSteps) => prevSteps.filter((o) => o.id !== id));
-  }
+  };
 
   const handleEdit = (id) => {
     const step = steps.find((o) => o.id === id);
     setEditingStepID(step.id);
-    setForm({ date: moment(step.date).format('DD.MM.YY'), distance: step.distance });
-  }
+    setForm({
+      date: moment(step.date).format("DD.MM.YYYY"),
+      distance: step.distance,
+    });
+  };
 
   return (
     <div className='steps'>
@@ -58,7 +70,7 @@ export default function Steps(props) {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
-      <StepList steps={steps} onRemove={handleRemove} onEdit={handleEdit}/>
+      <StepList steps={steps} onRemove={handleRemove} onEdit={handleEdit} />
     </div>
-  )
+  );
 }
