d24f0dd1fbe428e992d7546639aa283af93be678
romank123 <67678390+romank123@users.noreply.github.com>
2022-11-10 23:28:55 +0300

    add homework-6-cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import './App.css';
-import Steps from './Steps/Steps';
+import React from "react";
+import Card from "./components/Card/Card";
+import "./App.css";
+
+const cards = [
+  {
+    img: "logo512.png",
+    text: `Some quick example text to build on the card title and make up the bulk of thr card's content`,
+  },
+  {
+    title: "Special title treatment",
+    text: "With supporting text below as a natural lead-in to additional content",
+  },
+];
 
 function App() {
-  return <Steps/>;
+  return (
+    <React.Fragment>
+      {cards.map((o, index) => (
+        <Card img={o.img} key={index}>
+          {<h5 className='card-title'>{o.title || "Card title"}</h5>}
+          {o.text && <p className='card-text'>{o.text}</p>}
+          {
+            <a href={o.btnhref || "#"} className='btn btn-primary'>
+              {o.btnText || "Go somewere"}
+            </a>
+          }
+        </Card>
+      ))}
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/Steps/StepModel.js
+++
@@ -1,9 +0,0 @@
-import {nanoid} from 'nanoid';
-
-export default class StepModel {
-  constructor(date, distance) {
-    this.id = nanoid();
-    this.date = date;
-    this.distance = distance;
-  }
-}
