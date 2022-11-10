e8714d8397c63722922b35a5ebd13fedc2d11959
romank123 <67678390+romank123@users.noreply.github.com>
2022-11-10 23:31:56 +0300

    add homework-7-decomposition

--- src/App.js
+++ src/App.js
@@ -1,33 +1,81 @@
-import React from "react";
-import Card from "./components/Card/Card";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Card from './components/Card/Card';
+import SearchForm from './components/SearchForm/SearchForm';
+import WeatherCard from './components/WeatherCard/WeatherCard';
+import InlineCard from './components/InlineCard/InlineCard';
+import News from './components/News/News';
 
-const cards = [
-  {
-    img: "logo512.png",
-    text: `Some quick example text to build on the card title and make up the bulk of thr card's content`,
-  },
-  {
-    title: "Special title treatment",
-    text: "With supporting text below as a natural lead-in to additional content",
-  },
+const newsSections = [
+  { text: 'Сейчас в СМИ', faded: false },
+  { text: 'в Германии', faded: true },
+  { text: 'Рекомендуем', faded: true },
+];
+
+const news = [
+  { img: 'img1', text: 'news1'},
+  { img: 'img2', text: 'news2'},
+  { img: 'img3', text: 'news3'},
+  { img: 'img4', text: 'news4'},
+  { img: 'img5', text: 'news5'},
+];
+
+const currencies = [
+  { title: 'USD MOEX 63,52', text2: '+0.09' },
+  { title: 'EUR MOEX 70.86', text2: '+0.14' },
+  { title: 'НЕФТЬ 64,90', text2: '+1,63%' },
+  { text2: '...' },
+];
+
+const topRightCard = { 
+  img: 'big',
+  title: 'Работа над ошибками',
+  text: 'Смотрите на Яндексе и запоминайте',
+};
+
+const products = [
+  { text: 'Видео', href: '#' },
+  { text: 'Картинки', href: '#' },
+  { text: 'Новости', href: '#' },
+  { text: 'Карты', href: '#' },
+  { text: 'Маркет', href: '#' },
+  { text: 'Переводчик', href: '#' },
+  { text: 'Эфир', href: '#' },
+  { text: 'еще', href: '#' },
 ];
 
 function App() {
+  const [value, setValue] = React.useState('');
   return (
-    <React.Fragment>
-      {cards.map((o, index) => (
-        <Card img={o.img} key={index}>
-          {<h5 className='card-title'>{o.title || "Card title"}</h5>}
-          {o.text && <p className='card-text'>{o.text}</p>}
-          {
-            <a href={o.btnhref || "#"} className='btn btn-primary'>
-              {o.btnText || "Go somewere"}
-            </a>
-          }
-        </Card>
-      ))}
-    </React.Fragment>
+    <div className="App">
+      <div className='header'>
+        <News {...{newsSections, news, currencies}}/>
+        <Card {...topRightCard} style={{width: '15rem'}}/>
+      </div>
+      
+      <div className='search'> {/* можно завернуть в компонент */}
+        <img className='' src='' alt='Яндекс'/>
+        <div className='search-body'>
+          <div className='search-links'>
+            {products.map((o, index) =>
+              <a className={`h4 ${o.faded && 'faded'}`} href={o.href || '#'} key={index}>{o.text}</a>
+            )}
+          </div>
+          <SearchForm className='form' value={value} onChange={(value) => setValue(value)} onSubmit={()=>{}}/>
+          <div className='search-footer'>
+            <span>{'Найдется всё. Например, '}</span>
+            <span className='faded'>{'фаза луны сегодня'}</span>
+          </div>
+        </div>
+      </div>
+
+      <img className='banner' src='' alt=''/>
+
+      <div className='footer'>
+        <WeatherCard/>
+        {/* Прочие специфичные карточки, содержащие InlineCard*/}
+      </div>
+    </div>
   );
 }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import './Card.css'
 
-function Card({ img, ...props }) {
+/**
+ * Компонент - карточка с картинкой, заголовком,
+ * текстом, доп текстом и ссылкой
+ */
+function Card({ img, title, text, text2, href, className, ...props }) {
   return (
-    <div className='card' style={{ width: '18rem' }}>
-      { img && <img src={img} className="card-img-top" alt="..."/> }
-      <div className="card-body">
-        {props.children}
-      </div>
+    <div {...props} className={(className || '') + ' card'}>
+      <a href={href || '#'}>
+        {img && <img className='card-image' src='img' alt=''/>}
+        {title && <div className='card-title'>{title}</div>}
+        {text && <div className='card-text'>{text}</div>}
+        {text2 && <div className='card-text2'>{text2}</div>}
+      </a>
     </div>
-  )
+  );
 }
 
-Card.propTypes = {
-  img: PropTypes.string,
-}
-
-export default Card
-
+export default Card;
